adc50247dd5d84a4ba8236e9047e6122aff30fc4
wilztan <awz77wil@gmail.com>
2018-06-10 19:29:13 +0800

    fixing message

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function getWeather(city) {
   .then(response => {
     var weather = response.data;
     console.log(JSON.stringify(weather));
-    var content = "Hi Everyone, Today's Weather will be "+response.data.weather[0].description+", Humidity of "+response.data.main.humidity+", with temperature of "+((response.data.main.temp)/10)toFixed(1)+" C";
+    var content = "Hi Everyone, Today's Weather will be "+response.data.weather[0].description+", Humidity of "+response.data.main.humidity+", with temperature of "+((response.data.main.temp)/10).toFixed(1)+" C";
     var newMessage ={
       user:'LORD ADMIN',
       date: dt.getHours()+":"+dt.getMinutes(),
